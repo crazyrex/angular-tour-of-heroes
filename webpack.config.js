dc2f0207399dc5902f1d22abd77cf1a7c3e61def
sebawita <sebawita@gmail.com>
2018-11-02 18:47:37 -0700

    Update to {N} 5.0

--- webpack.config.js
+++ webpack.config.js
@@ -44,11 +44,6 @@ module.exports = env => {
         sourceMap, // --env.sourceMap
         hmr, // --env.hmr,
     } = env;
-
-    env.externals= env.externals || []
-    env.externals.push("nativescript-angular");
-    env.externals.push("@angular");
-
     const externals = (env.externals || []).map((e) => { // --env.externals
         return new RegExp(e + ".*");
     });
