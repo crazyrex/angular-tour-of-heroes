471df871ab5be96440da9f3a204877122b3cc49c
sebawita <sebawita@gmail.com>
2018-11-02 18:48:21 -0700

    clean up

--- src/app/app.component.tns.ts
+++ src/app/app.component.tns.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-// import * as app from 'tns-core-modules/application';
-// import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
-  // closeDrawer() {
-  //   const sideDrawer = <RadSideDrawer>app.getRootView();
-  //   sideDrawer.closeDrawer();
-  // }
-}
+export class AppComponent { }
